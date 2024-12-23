171442a88499221d86cd1e479c0dd76f578eb31b
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-12-23 12:39:17 +0530

    added apis and tanstack hooks

---
+++ src/api/auth/index.js
@@ -0,0 +1,26 @@
+import axios from '@/config/axiosConfig.js'
+
+export const signUpRequest = async ({ email, password, username }) => {
+      try {
+            const response = await axios.post("/users/signup", {
+                  email, password, username
+            })
+            return response.data
+
+      } catch (error) {
+            console.error(error)
+
+      }
+},
+export const signInRequest = async ({ email, password }) => {
+      try {
+            const response = await axios.post("/users/signin", {
+                  email, password
+            })
+            return response.data
+
+      } catch (error) {
+            console.error(error)
+
+      }
+}
---
+++ src/hooks/apis/auth/useSignup.js
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { signUpRequest } from '@/api/auth';
+export const useSignUp=()=>{
+      const{isPending,error,isSuccess,mutate:signUpMutation}=useMutation({
+            mutationFn:signUpRequest,
+            onSuccess:()=>{
+                  console.log('successfully sign up',data);
+                  
+            },
+            onError:()=>{
+                  console.log('failed!! to sign up',error);
+                  
+            }
+      });
+
+      return{
+            isPending,
+            error,
+            isSuccess,
+            signUpMutation
+      };
+};
