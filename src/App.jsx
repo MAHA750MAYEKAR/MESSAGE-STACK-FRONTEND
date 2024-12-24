c12b0f7d1eb94d6d79e3af3dcbb3e4eb16f11ce8
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-12-24 21:06:59 +0530

    added toaster

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { SignInCard } from "./components/organisms/Auth/signInCard";
 import Auth from "./pages/auth/Auth";
 import { NotFound } from "./pages/NotFound/NotFound";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Toaster } from "@/components/ui/toaster";
 function App() {
   const queryClient = new QueryClient();
   return (
@@ -31,6 +31,7 @@ function App() {
           />
           <Route path="/*" element={<NotFound />} />
         </Routes>
+        <Toaster />
       </QueryClientProvider>
     </>
   );
--- src/hooks/apis/auth/useSignup.js
+++ src/hooks/apis/auth/useSignup.js
@@ -1,22 +1,41 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
 
-import { signUpRequest } from '@/api/auth';
+import { signUpRequest } from "@/api/auth";
+import { useToast } from "@/hooks/use-toast";
+
 export const useSignUp = () => {
-     
-      const{isPending,error,isSuccess,mutateAsync:signUpMutation}=useMutation({
-            mutationFn:signUpRequest,
-            onSuccess:()=>{
-                  console.log('successfully sign up');                  
-            },
-            onError:(error)=>{
-                  console.log('failed!! to sign up',error);                  
-            }
-      });
+  const { toast } = useToast();
 
-      return{
-            isPending,
-            error,
-            isSuccess,
-            signUpMutation
-      };
+  const {
+    isPending,
+    error,
+    isSuccess,
+    mutateAsync: signUpMutation,
+  } = useMutation({
+    mutationFn: signUpRequest,
+    onSuccess: () => {
+      toast({
+        title: "Successfully signed up",
+        message: "You will be redirected to the login page in a few seconds",
+        type: "success",
+      });
+      console.log("successfully sign up");
+    },
+    onError: (error) => {
+      toast({
+        title: "Failed to sign up",
+        message: error.message,
+        type: "error",
+        variant: "destructive",
+      });
+      console.log("failed!! to sign up", error);
+    },
+  });
+
+  return {
+    isPending,
+    error,
+    isSuccess,
+    signUpMutation,
+  };
 };
